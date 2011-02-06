303fa9da8aba3b3a0cc4d5cbeea215f86289f71e
Harald Hauknes <harald@hauknes.org>
2011-02-06 17:21:58 +0100

    debug again

--- iRepair.lua
+++ iRepair.lua
@@ -7,7 +7,7 @@ function GetLowestDurabilityItemAndSlot()
   for i, slot in ipairs(slots_to_check) do
     durmax, durnow = GetInventoryItemDurability(GetInventorySlotInfo(slot))
     if durmax then -- Check if it return a nil value, happens if no item
-      -- print(slot.." "..durmax.." now "..durnow) -- used for debugging
+      print(slot.." "..durmax.." now "..durnow) -- used for debugging
       percent = (durmax / durnow) * 100
       if (percent < lowest) then
         lowest = percent
